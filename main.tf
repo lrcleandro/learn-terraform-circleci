0864bef1a8ab1578e96c1093794df8ce50a1619a
Leandro Ribeiro Calixto <lrcleandro@gmail.com>
2022-02-14 22:45:40 -0300

    Add remote backend and variable definitions

--- main.tf
+++ main.tf
@@ -12,6 +12,12 @@ provider "aws" {
   region = var.region
 }
 
+backend "s3" {
+   bucket = "circle-ci-backend-20220215013957114500000001"
+   key    = "terraform/webapp/terraform.tfstate"
+   region = "us-east-1"
+}
+
 resource "random_uuid" "randomid" {}
 
 resource "aws_iam_user" "circleci" {
